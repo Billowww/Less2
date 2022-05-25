ff72f0e23366146053b6925f0cc71ccc6ac3bed6
Billow <kolyabontsevich@gmail.com>
2022-05-25 15:49:01 +0300

    I'am

--- It_academia/src/main/java/by/it_academy/lesson2/Main.java
+++ It_academia/src/main/java/by/it_academy/lesson2/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+int Weight = 72;
+int Age = 20;
+float height = 1.8f;
+System.out.println("My weight is " + Weight + /n "My age is " + Age + /n "My height is " + height);
     }
 }

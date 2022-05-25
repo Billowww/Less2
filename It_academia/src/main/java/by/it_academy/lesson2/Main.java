00463fa37ba11e5d875a33b453724a090935bd1c
Billow <kolyabontsevich@gmail.com>
2022-05-25 16:36:23 +0300

    Сorrected code

--- It_academia/src/main/java/by/it_academy/lesson2/Main.java
+++ It_academia/src/main/java/by/it_academy/lesson2/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-int Weight = 72;
-int Age = 20;
+int weight = 72;
+int age = 20;
 float height = 1.8f;
-System.out.println("My weight is " + Weight + /n "My age is " + Age + /n "My height is " + height);
+System.out.println("My weight is "\n + Weight +  "My age is "\n + Age +  "My height is "\n + height);
     }
 }

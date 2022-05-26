44c45839fb6cd272c586bdbecbbb731f5c4bc5f2
Billow <kolyabontsevich@gmail.com>
2022-05-26 12:18:58 +0300

    I corrected all code

--- Practice 1/src/main/java/by/it_academy/lesson2/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-int weight = 72;
-int age = 20;
-float height = 1.8f;
-System.out.println("My weight is "\n + Weight +  "My age is "\n + Age +  "My height is "\n + height);
-    }
-}
---
+++ src/main/java/by/it_academy/lesson2/Main.java
@@ -0,0 +1,12 @@
+package by.it_academy.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        int weight = 72;
+        int age = 20;
+        float height = 1.8f;
+        System.out.println("My weight is " + weight);
+        System.out.println("My age is " + age);
+        System.out.println("My height is " + height);
+    }
+}

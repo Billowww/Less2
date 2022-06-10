0f33bfa529d35f71f476514c01ed3fe11506b7c6
Billowww <kolyabontsevich@gmail.com>
2022-06-10 18:48:43 +0300

    correct

--- src/main/java/by/it_academy/lesson7/Babla.java
+++ src/main/java/by/it_academy/lesson7/Babla.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Babla {
     public static void main(String[] args) {
-        String text = "acnoc od tertoms leyxa ayI";
+        String text = "Its nice";
         String[] array =text.split("");
         for(int i = 0; i<1;i++){
             for(int j = array.length-1;j>=i ;j-- ){
